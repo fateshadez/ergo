796eff4d64fe81cf799d1faf56af0308382479f1
fateshadez <givelokca@gmail.com>
2026-06-14 12:56:44 +0300

    Change variable names

--- app/config/database.php
+++ app/config/database.php
@@ -1,6 +1,9 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'task-app');
+define('DB_HOST', getenv('MYSQLHOST') ?: 'localhost');
+define('DB_PORT', getenv('MYSQLPORT') ?: '3306');
+define('DB_USER', getenv('MYSQLUSER') ?: 'root');
+define('DB_PASS', getenv('MYSQLPASSWORD') ?: '');
+define('DB_NAME', getenv('MYSQLDATABASE') ?: 'task-app');
+
+define('BASE_URL', '');
