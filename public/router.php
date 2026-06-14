aba40b4a87f9f92bd00d0dea191ab8e9bb4e828a
fateshadez <givelokca@gmail.com>
2026-06-14 15:37:07 +0300

    Debug output

--- public/router.php
+++ public/router.php
@@ -1,4 +1,7 @@
 <?php
+error_log('ROUTER START');
+error_log('REQUEST_URI: ' . $_SERVER['REQUEST_URI']);
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $file = __DIR__ . $path;
 
@@ -8,9 +11,6 @@ if (is_file($file)) {
 
 // Pass the URL to index.php
 $_GET['url'] = ltrim($path, '/');
-
-error_log('Raw REQUEST_URI: ' . $_SERVER['REQUEST_URI']);
-error_log('Parsed path: ' . $path);
 error_log('GET url set to: ' . $_GET['url']);
 
 require_once __DIR__ . '/index.php';
