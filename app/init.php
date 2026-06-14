cc9c87f99f37874412e833e9d8ec2f50e430f481
fateshadez <givelokca@gmail.com>
2026-06-14 15:12:14 +0300

    Debug php session

--- app/init.php
+++ app/init.php
@@ -1,6 +1,9 @@
 <?php
 
 session_start();
+echo 'logged_in: ' . (isset($_SESSION['logged_in']) ? 'true' : 'false') . '<br>';
+echo 'url: ' . $_SERVER['REQUEST_URI'] . '<br>';
+die();
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/core/Error.php';
 require_once __DIR__ . '/core/Model.php';
