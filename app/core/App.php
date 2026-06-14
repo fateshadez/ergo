1b4a49ec9847d57a4599683fa1ae9dd54733d2a0
fateshadez <givelokca@gmail.com>
2026-06-14 13:16:34 +0300

    Add handling for error 404

--- app/core/App.php
+++ app/core/App.php
@@ -11,27 +11,30 @@ class App
   {
     $url = $this->parseUrl();
 
-    if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+    if (file_exists(__DIR__ . '/../controllers/' . $url[0] . '.php')) {
       $this->controller = $url[0];
       unset($url[0]);
+    } else {
+      $this->show404();
+      return;
     }
 
-    require_once '../app/controllers/' . $this->controller . '.php';
-
+    require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
     $this->controller = new $this->controller;
 
     if (isset($url[1])) {
       if (method_exists($this->controller, $url[1])) {
         $this->method = $url[1];
         unset($url[1]);
+      } else {
+        $this->show404();
+        return;
       }
     }
 
     $this->params = $url ? array_values($url) : [];
-
     call_user_func_array([$this->controller, $this->method], $this->params);
   }
-
   public function parseUrl()
   {
     if (isset($_GET['url'])) {
@@ -42,11 +45,17 @@ class App
 
     if ($url === ['auth'] || $url === ['auth', 'register']) {
       if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-        header('Location: /task-app/home');
+        header('Location: /home');
         exit();
       }
     }
-
     return $url;
+  }
+
+  private function show404()
+  {
+    http_response_code(404);
+    require_once __DIR__ . '/../views/errors/404.php';
+    exit();
   }
 }
