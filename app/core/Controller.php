9b50bde11113cfa194a4861c5ce06dc8a3f660dd
fateshadez <givelokca@gmail.com>
2026-06-14 13:54:40 +0300

    Update paths

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,14 +3,14 @@ class Controller
 {
   public function model($model)
   {
-    require_once '../app/models/' . $model . '.php';
+    require_once __DIR__ . '/../models/' . $model . '.php';
     return new $model();
   }
 
   public function view($view, $data = [])
   {
     extract($data);
-    require_once '../app/views/' . $view . '.php';
+    require_once __DIR__ . '/../views/' . $view . '.php';
   }
 
   protected function isLoggedIn()
