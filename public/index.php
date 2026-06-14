1924dc39d1a9642f37476ae3fabf91606c51c242
fateshadez <givelokca@gmail.com>
2026-06-14 13:19:24 +0300

    Add handling for error 500

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
-
-require_once "../app/init.php";
-
-$app = new App();
+require_once '../app/init.php';
+try {
+    $app = new App();
+} catch (Exception $e) {
+    http_response_code(500);
+    require_once '../app/views/errors/500.php';
+}
