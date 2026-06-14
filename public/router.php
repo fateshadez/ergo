92ea9156ea95ad836c1605f082b5f3769568ad5a
fateshadez <givelokca@gmail.com>
2026-06-14 16:38:16 +0300

    Handle root path in router

--- public/router.php
+++ public/router.php
@@ -1,16 +1,18 @@
 <?php
-error_log('ROUTER START');
+error_log('=== ROUTER START ===');
 error_log('REQUEST_URI: ' . $_SERVER['REQUEST_URI']);
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $file = __DIR__ . $path;
 
 if (is_file($file)) {
-    return false; // serve static files (css, js, images) directly
+    return false;
 }
 
-// Pass the URL to index.php
-$_GET['url'] = ltrim($path, '/');
-error_log('GET url set to: ' . $_GET['url']);
+$url = ltrim($path, '/');
+
+$_GET['url'] = empty($url) ? 'auth' : $url;
+
+error_log('GET url: ' . $_GET['url']);
 
 require_once __DIR__ . '/index.php';
