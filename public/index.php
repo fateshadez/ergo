24bd0f27bff8b15623c6c2249983170c694c1866
fateshadez <givelokca@gmail.com>
2026-06-14 13:45:54 +0300

    Enable error display for debug

--- public/index.php
+++ public/index.php
@@ -1,8 +1,12 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once '../app/init.php';
 try {
     $app = new App();
 } catch (Exception $e) {
     http_response_code(500);
-    require_once '../app/views/errors/500.php';
+    echo $e->getMessage();
+    // require_once '../app/views/errors/500.php';
 }
