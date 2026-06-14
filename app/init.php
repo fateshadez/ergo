7a308cc768dcf5648460bf05a6c2a2df07f12a08
fateshadez <givelokca@gmail.com>
2026-06-14 14:12:04 +0300

    Change file name

--- app/init.php
+++ app/init.php
@@ -2,7 +2,7 @@
 
 session_start();
 require_once __DIR__ . '/config/database.php';
-require_once __DIR__ . '/core/FormError.php';
+require_once __DIR__ . '/core/ErrorLog.php';
 require_once __DIR__ . '/core/Model.php';
 require_once __DIR__ . '/core/Controller.php';
 require_once __DIR__ . '/core/App.php';
