37a8c4ba797117d94a9ecdd1b58e7801c5c25a14
fateshadez <givelokca@gmail.com>
2026-06-14 16:43:32 +0300

    Clear up debug logs

--- public/index.php
+++ public/index.php
@@ -1,15 +1,12 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
 chdir(dirname(__DIR__));
 
 require_once 'app/init.php';
 
 try {
-    $app = new App();
+  $app = new App();
 } catch (Exception $e) {
-    http_response_code(500);
-    echo $e->getMessage();
-    echo $e->getFile() . ' on line ' . $e->getLine();
+  http_response_code(500);
+  require_once 'app/views/errors/500.php';
 }
