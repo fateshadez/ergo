7fb83fce6dda618576edd36ce577e64f1cc966db
fateshadez <givelokca@gmail.com>
2026-06-14 15:20:31 +0300

    Capitilize file name to match class name

--- app/core/App.php
+++ app/core/App.php
@@ -20,7 +20,7 @@ class App
     }
 
     require_once __DIR__ . '/../controllers/' . $this->controller . '.php';
-    $this->controller = new $this->controller;
+    $this->controller = new (ucfirst($this->controller))();
 
     if (isset($url[1])) {
       if (method_exists($this->controller, $url[1])) {
