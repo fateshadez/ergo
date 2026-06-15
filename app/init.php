660e33fae2f7ce839d84198b8261959f2c787202
fateshadez <104824751+fateshadez@users.noreply.github.com>
2026-06-15 10:06:21 +0300

    Connect Helper.php to init.php

--- app/init.php
+++ app/init.php
@@ -6,3 +6,4 @@ require_once __DIR__ . '/core/Error.php';
 require_once __DIR__ . '/core/Model.php';
 require_once __DIR__ . '/core/Controller.php';
 require_once __DIR__ . '/core/App.php';
+require_once __DIR__ . '/core/Helper.php';
